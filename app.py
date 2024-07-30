857401f22ade956fb093362d47ab9244a8222b69
Molly <mollymuthama@gmail.com>
2024-07-30 23:10:09 +0300

    Migrations

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from models import db, Member, Game
-#from config import Config
-#from flask_migrate import Migrate
+from config import Config
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
-    #app.config.from_object(Config)
+    app.config.from_object(Config)
     db.init_app(app)
     return app
 
 app = create_app()
-#migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
