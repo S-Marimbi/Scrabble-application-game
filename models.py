13fad45d50d9b323b0f946226d700934b7bff526
Molly <mollymuthama@gmail.com>
2024-07-31 17:24:05 +0300

    init

--- models.py
+++ models.py
@@ -1,15 +1,15 @@
-from flask_sqlalchemy import SQLalchemy, Model
+from flask_sqlalchemy import SQLAlchemy, model
 
-db = SQLalchemy()
+db = SQLAlchemy()
 
 class Member(db.Model):
     __tablename__ = 'member'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.varchar(100), nullable=False)
-    email = db.Column(db.varchar(200), nullable=False, unique=True)
-    password = db.Column(db.varchar(100), nullable=False)
-    game = db.relationship('Game',backref='member',uselist=False,cascade='all, delete-orphan')
+    user_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    game = db.Relationship('Game',backref='member',uselist=False,cascade='all, delete-orphan')
 
     def info(self):
         return{'id':self.id, 'user_name':self.user_name, 'email':self.email}
@@ -18,7 +18,7 @@ class Game(db.Model):
     __tablename__ = 'game'
     
     id = db.Column(db.Integer, primary_key=True)
-    board = db.Column(db.text, nullable=False)
-    player_rack = db.Column(db.text, nullable=False)
-    computer_rack = db.column(db.text, nullable=False)
+    board = db.Column(db.Text, nullable=False)
+    player_rack = db.Column(db.Text, nullable=False)
+    computer_rack = db.Column(db.Text, nullable=False)
     member_id = db.Column(db.Integer, db.ForeignKey('member.id', ondelete = 'CASCADE'), nullable=False, unique = True)
